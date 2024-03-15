76edffcd8fd44911e093c7b1f8e05621876ce51e
e60e256 <e60e256@gmail.com>
2024-03-15 08:27:15 +0000

    added scikit-build

--- setup.py
+++ setup.py
@@ -5,9 +5,23 @@ class BinaryDistribution(Distribution):
     def has_ext_modules(foo):
         return True
 
+import io
+import os
+import re
+import shlex
+import sys
+from setuptools import find_packages
+try:
+	from skbuild import setup
+except ImportError:
+	print("Please update pip to pip 10 or greater, or a manually install the PEP 518 requirements in pyproject.toml", file=sys.stderr)
+	raise
+
 setup(
-    name='qdd',
-    packages=['qdd'],
-    include_package_data=True,
+    packages=find_packages(where="qdd"),
+	package_dir={'': "qdd"},
+	include_package_data=True,
+	cmake_args=['-DCMAKE_BUILD_TYPE=Release', '-DCMAKE_VERBOSE_MAKEFILE=1'],
+	cmake_install_dir="qdd",
     distclass=BinaryDistribution,
 )
